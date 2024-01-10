1562b6ee266a678e70c481b0588b73ae95d1a2c7
Eduard Latypov <LatypovEdik1988@yandex.ru>
2024-01-10 17:18:21 +0300

    Дополнение к коммиту, где была реализована функция восстановления пароля путем использования SMTP сервера

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
+import datetime
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    photo = models.ImageField(
+        upload_to="users/%Y/%m/%d/",
+        default="users/default.jpg",
+        blank=True,
+        null=True,
+        verbose_name="Фотография",
+    )
+    date_birth = models.DateTimeField(
+        blank=True, null=True, verbose_name="Дата рождения"
+    )

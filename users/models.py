0ee70cbacc7224ce063c53b10fd8da51b6901e22
Eduard Latypov <LatypovEdik1988@yandex.ru>
2024-01-10 18:08:46 +0300

    Переопределил модель Юзера, добавил возможность загружать аватарку и указывать дату рождения

--- users/models.py
+++ users/models.py
@@ -3,11 +3,12 @@ import datetime
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from sitewomen import settings
+
 
 class User(AbstractUser):
     photo = models.ImageField(
         upload_to="users/%Y/%m/%d/",
-        default="users/default.jpg",
         blank=True,
         null=True,
         verbose_name="Фотография",
--- users/views.py
+++ users/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, UpdateView
 
+from sitewomen import settings
 from .forms import (
     LoginUserForm,
     RegisterUserForm,
@@ -31,7 +32,10 @@ class UserRegister(CreateView):
 class UserProfile(LoginRequiredMixin, UpdateView):
     form_class = ProfileUserForm
     template_name = "users/profile.html"
-    extra_context = {"title": "Профиль пользователя"}
+    extra_context = {
+        "title": "Профиль пользователя",
+        "default_image": settings.DEFAULT_USER_IMAGE,
+    }
 
     def get_success_url(self):
         return reverse_lazy("users:profile")

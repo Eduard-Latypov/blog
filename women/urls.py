e8a75d714d857826d4106293e7f37e7f5f31bbb8
Eduard Latypov <LatypovEdik1988@yandex.ru>
2024-01-18 10:39:31 +0300

    главная страница закеширована и теперь отдается без запросов к БД

--- women/urls.py
+++ women/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, re_path, register_converter
+from django.views.decorators.cache import cache_page
+
 from . import views
 from . import converters
 
@@ -6,7 +8,9 @@ from . import converters
 register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
-    path("", views.WomenHome.as_view(), name="home"),  # http://127.0.0.1:8000
+    path(
+        "", cache_page(60 * 5)(views.WomenHome.as_view()), name="home"
+    ),  # http://127.0.0.1:8000
     path("about/", views.about, name="about"),
     path("addpage/", views.AddPage.as_view(), name="add_page"),
     path("contact/", views.ContactFormView.as_view(), name="contact"),
